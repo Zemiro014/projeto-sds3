71022b512f11cdacb71e863dc22224c0ab2e5e15
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-05-06 18:23:28 -0300

    Group by search

--- backend/src/main/java/com/devjeronimo/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/devjeronimo/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.devjeronimo.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devjeronimo.dsvendas.dto.SaleDTO;
+import com.devjeronimo.dsvendas.dto.SaleSuccessDTO;
+import com.devjeronimo.dsvendas.dto.SaleSumDTO;
 import com.devjeronimo.dsvendas.entities.Sale;
 import com.devjeronimo.dsvendas.repositories.SaleRepository;
 import com.devjeronimo.dsvendas.repositories.SellerRepository;
@@ -26,5 +30,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = saleRepo.findAll(pageable);
 		return result.map(seller -> new SaleDTO(seller));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return saleRepo.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return saleRepo.successGroupedBySeller();
 	}
 }
